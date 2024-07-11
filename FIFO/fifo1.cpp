9e43308abe315bba60a3c261d1611a025c386339
Renee <reneepaxson@gmail.com>
2024-07-11 16:18:17 -0400

    fifo code done

--- FIFO/fifo1.cpp
+++ FIFO/fifo1.cpp
@@ -9,21 +9,24 @@
 #include <time.h>
 
 int main() {
-    // resets the default umask bits so that 
+    // reset the default umask bits so that 
     // it doesn't mess with the permissions on the fifo
     umask(0000);
 
     // int to refer to the number in the file descriptor table
+    // most people just call this variable 'fd'
     int fileDescriptor = -1;
     
     // FIFOs are stored as files 
+    // This is the file path to where we wish to store our FIFO 
+    // and how we wish to name it
     char * myfifo = "./myfifo"; 
 
-    // make fifo with RW permissions for all UGO
+    // make fifo with RW permissions for all
     mkfifo(myfifo, 0666);
 
     // open fifo for write only 
-    // will hold here until the read end of the pipe is open
+    // program will hold here until the read end of the pipe is open in fifo2
     while (fileDescriptor == -1) fileDescriptor = open(myfifo, O_WRONLY, 0666);
     
     // buffer to hold the data to send through the pipe
@@ -35,6 +38,7 @@ int main() {
         // fill buffer with random numbers
         buff[i] = rand();
     }
+
     // write buff to the fifo 
     write(fileDescriptor, buff, sizeof(buff)); 
 
--- FIFO/fifo2.cpp
+++ FIFO/fifo2.cpp
@@ -9,31 +9,36 @@
 #include <iostream>
 
 int main() {
-    // resets the default umask bits so that 
+    // reset the default umask bits so that 
     // it doesn't mess with the permissions on the fifo
     umask(0000);
 
     // int to refer to the number in the file descriptor table
+    // most people just call this variable 'fd'
     int fileDescriptor = -1;
     
-    // FIFOs are stored as files 
+    // FIFOs are stored as files
+    // This is the file path to where we wish to store our FIFO 
+    // and how we wish to name it
     char * myfifo = "./myfifo"; 
 
-    // make fifo with RW permissions for all UGO
+    // make fifo with RW permissions for all
     mkfifo(myfifo, 0666);
 
     // open fifo for read only 
-    // will hold here until the write end of the pipe is open
+    // program will hold here until the write end of the pipe is open in fifo1
     while (fileDescriptor == -1) fileDescriptor = open(myfifo, O_RDONLY, 0666);
     
     // buffer to hold the data sent through the pipe
-    int buff[100]; 
-
-
+    // size 1 since we read one entry at a time
+    int buff[1]; 
+    
+    // `read` will return -1 if there's an error  
+    // and 0 if the buffer is empty
+    // and holds indefinitely if the buffer hasn't been written to yet
     while (read(fileDescriptor, buff, sizeof(buff)) > 0) {
-        for (int i = 0; i < 100; i++) {
-            std::cout << buff[i]<< std::endl;
-        }
+        // print each buffer entry to the terminal
+        std::cout << buff[0] << std::endl;
     }
     
     
